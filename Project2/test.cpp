c3d5de66b58ad55ca08811a2bd0b14b714bf5c3c
lyf199293 <307576392@qq.com>
2025-04-30 18:57:51 +0800

    添加项目文件。

---
+++ Project2/test.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include<Windows.h>
+#include<string>
+using namespace std;
+
+void clearBuff() {
+	char tmp;
+	while (tmp = getchar() != '\n');
+}
+int main() {
+	string s;
+	int count=0;
+	int len=0;
+
+	cout << "请输入多行" << endl;
+	while (1)
+	{
+		if ((bool)getline(cin, s)==0){
+			break;
+		}
+		count++;
+		len += s.length();
+
+	}
+	cout <<"count is"<< count <<" len is"<< len << endl;
+	
+	system("pause");
+
+	return 0;
+}
---
+++ Project2/test1.c
@@ -0,0 +1,21 @@
+﻿
+#include<stdio.h>
+#include<string.h>
+#pragma warning(disable:4996)
+int main1() {
+	char word[128];
+	int count = 0;
+	int length = 0;
+	
+	printf("请输入:\n");
+	while (1)
+	{
+		if (gets(word)==0) {
+			break;
+		}
+		count++;
+		length += strlen(word);
+	}
+	printf("count =%d,length=%d\n", count, length);
+	return 0;
+}
